3845fbe924f86053ad5eaeb125f33334840d181e
samir-kamble <samir.kamble@gmail.com>
2016-09-23 19:50:05 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 if (navigator.serviceWorker) {
      navigator.serviceWorker.register('./service-worker.js', {scope: './'})
         .then(function (registration) {
-            console.log('Registration Successful');
+            console.log('[AppJS] Registration Successful');
            // console.log(registration);
         })
         .catch(function (e) {
             console.error(e);
         })
 } else {
-    console.log('Service Worker is not supported in this browser.')
+    console.log('[AppJS] Service Worker is not supported in this browser.')
 }
